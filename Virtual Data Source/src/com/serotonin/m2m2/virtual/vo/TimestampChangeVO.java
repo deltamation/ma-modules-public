a48434a53aea3ee63b2d24e1519255d1bb573b44
tpacker <tpacker@ubuntu-aloha.hawaii.rr.com>
2013-06-24 13:15:58 -1000

    Adding Timestamp Change Types

---
+++ Virtual Data Source/src/com/serotonin/m2m2/virtual/vo/TimestampChangeVO.java
@@ -0,0 +1,53 @@
+/*
+    Copyright (C) 2013 Deltamation Software All rights reserved.
+    @author Terry Packer
+ */
+package com.serotonin.m2m2.virtual.vo;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import com.serotonin.m2m2.i18n.TranslatableMessage;
+import com.serotonin.m2m2.virtual.rt.ChangeTypeRT;
+import com.serotonin.m2m2.virtual.rt.TimestampChangeRT;
+
+/**
+ * Copyright (C) 2013 Deltamation Software. All Rights Reserved.
+ * @author Terry Packer
+ *
+ */
+public class TimestampChangeVO extends ChangeTypeVO{
+    public static final TranslatableMessage KEY = new TranslatableMessage("dsEdit.virtual.changeType.timestamp");
+
+    @Override
+    public int typeId() {
+        return Types.TIMESTAMP;
+    }
+
+    @Override
+    public TranslatableMessage getDescription() {
+        return KEY;
+    }
+
+    @Override
+    public ChangeTypeRT createRuntime() {
+        return new TimestampChangeRT();
+    }
+
+    //
+    //
+    // Serialization
+    //
+    private static final long serialVersionUID = -1;
+    private static final int version = 1;
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.writeInt(version);
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException {
+        in.readInt(); // Read the version. Value is currently not used.
+    }
+
+}
